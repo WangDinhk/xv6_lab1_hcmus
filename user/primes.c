6b229c0e030a6af03b7950918205f228d90e55c7
WangDinhk <22120433@student.hcmus.edu.vn>
2024-10-15 04:34:49 +0000

    update primes 2

--- user/primes.c
+++ user/primes.c
@@ -2,30 +2,60 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-#define MAX_NUM 280
+#define MAX 280
 
-void findPrimes() {
-    int numbers[MAX_NUM - 1];
-    int i, j, n = 0;
-
-    for (i = 2; i <= MAX_NUM; i++) {
-        numbers[n++] = i;
+void primes(int read_pipe) {
+    int p[2];
+    int num;
+    if (read(read_pipe, &num, sizeof(num)) <= 0) {
+        close(read_pipe); 
+        return;
     }
-
-    for (i = 0; i < n; i++) {
-        if (numbers[i] != 0) {
-            printf("prime %d\n", numbers[i]);
-            
-            for (j = i + 1; j < n; j++) {
-                if (numbers[j] % numbers[i] == 0) {
-                    numbers[j] = 0;
-                }
+    printf("prime %d\n", num);
+    if (pipe(p) < 0) {
+        printf("pipe creation failed\n");
+        exit(1);
+    }
+    int pid = fork();
+    if (pid < 0) {
+        printf("fork failed\n");
+        exit(1);
+    }
+    if (pid == 0) {
+        close(p[1]);  
+        close(read_pipe);  
+        primes(p[0]); 
+        close(p[0]);  
+    } else {
+        close(p[0]);  
+        int tmp;
+        while (read(read_pipe, &tmp, 4) > 0) {
+            if (tmp % num != 0) {
+                write(p[1], &tmp, 4);
             }
         }
+
+        close(p[1]);
+        close(read_pipe); 
+        wait(0); 
     }
 }
 
 int main(int argc, char *argv[]) {
-    findPrimes();
+    int p[2];
+    pipe(p);
+    int pid = fork();
+    if (pid == 0) {
+        close(p[1]);  
+        primes(p[0]); 
+        close(p[0]);  
+    } else {
+        close(p[0]); 
+        for (int i = 2; i <= 280; i++) {
+            write(p[1], &i, 4);  
+        }
+        close(p[1]);  
+        wait(0);  
+    }
     exit(0);
 }
