d897269ecd5f7cdd7e13a5d50997bda2af66b869
hVie1314 <1212hoangviet@gmail.com>
2024-12-29 12:20:44 +0700

    lab 3_3  pgtbl

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,8 @@
 #include "spinlock.h"
 #include "proc.h"
 
+#define MAX_PAGES 64  // Limit page number can check
+
 uint64
 sys_exit(void)
 {
@@ -75,6 +77,45 @@ int
 sys_pgaccess(void)
 {
   // lab pgtbl: your code here.
+  uint64 start_address; // Start virtual addr of page to be checked
+  int num_pages;        // Number of pages to be checked
+  uint64 user_result_address; // Addr in userspace us to store bitmask
+
+  // Get args from userspace 
+  argaddr(0, &start_address);
+  argint(1, &num_pages);      
+  argaddr(2, &user_result_address);
+
+  // Make sure numbers of pages in limination
+  if (num_pages < 0 || num_pages > MAX_PAGES)
+    return -1;
+
+  uint64 bitmask = 0; // Var to store result
+  struct proc *current_proc = myproc(); // Get curr process
+  pagetable_t pagetable = current_proc->pagetable; // Page table of curr process
+
+  // Loop for each page
+  for (int i = 0; i < num_pages; i++) {
+    // Calc virtual addr of curr page
+    uint64 current_va = start_address + i * PGSIZE;
+
+    // Get PTE (Page Table Entry) of virtual addr
+    pte_t *pte = walk(pagetable, current_va, 0);
+    if (pte == 0) // No PTE
+      continue;
+
+    // Check bit PTE_A in PTE (bit 6th)
+    // If the page has been accessed, bit 6th equal 1, else 0
+    if (*pte & PTE_A) {
+      bitmask |= (1L << i); // Store to bit i in bitmask mark page i has been accseed 
+      *pte &= ~PTE_A; // Clear the PTE_A bit so can check again next time
+    }
+  }
+
+  // Copy bitmask from kernel space to userspace 
+  if (copyout(pagetable, user_result_address, (char *)&bitmask, sizeof(bitmask)) < 0)
+    return -1;
+    
   return 0;
 }
 #endif
